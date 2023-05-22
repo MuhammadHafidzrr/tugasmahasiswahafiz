919f65d9fcbf2a240cad7f801dba8ebe2b0d7a25
MuhammadHafidz <111264621+MuhammadHafidzrr@users.noreply.github.com>
2023-05-22 12:02:07 +0700

    Add files via upload

---
+++ proses_pendaftaran.php
@@ -0,0 +1,43 @@
+<?php
+class Mahasiswa  {
+    public $nama ="";
+    public $email="";
+    public $password="";
+
+    function __construct($nama,$email,$password){
+         $this->nama = $nama ;
+         $this->email = $email;
+         $this->password = $password ;
+    }
+    function tampilBiodata(){
+       echo $this->nama ."<br />";
+       echo  $this->email."<br />" ;
+        echo $this->password."<br />";
+    }
+    function validasi(){
+        if($this->nama==""){
+            echo"nama kosong";
+        }
+        if($this->email==""){
+            echo"nama kosong";
+        }
+        if($this->password==""){
+            echo"nama kosong";
+        }
+        if($this->nama==""AND $this->password=="" AND $this->email==""){
+             return false;
+        }else{
+             return true;
+        }
+    }
+}
+
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$Mahasiswa = new Mahasiswa ($nama,$email,$password); 
+if($Mahasiswa->validasi()){ 
+    $Mahasiswa->tampilBiodata();
+}
+?>
